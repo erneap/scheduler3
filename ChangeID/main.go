d7e92e6542aa95e127405767ef72ec78915f716f
Anton Erne <antonerne@yahoo.com>
2025-08-06 12:31:59 +0900

    Aug06-1231

--- ChangeID/main.go
+++ ChangeID/main.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/erneap/go-models/v2/employees"
 	"github.com/erneap/go-models/v2/svcs"
+	"github.com/erneap/go-models/v2/users"
 	"github.com/erneap/models/v2/config"
-	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
@@ -20,74 +20,28 @@ func main() {
 	// to change a person's primary key,
 	// I must pull their employee record
 	// oldid = 68107f948a5f27b2e7cb8f60
-	email := "marcus.harkins.1.ctr@us.af.mil"
+	email := "fred.m.yelinek@rtx.com"
+	id := "6847a682a420d89795769e31"
 
 	// then add it with the new id.
-	user, err := svcs.GetUserByEMail(email)
+	emp, err := svcs.GetEmployee(id)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	// get old employee record
-	empCol := config.GetCollection(config.DB, "scheduler", "employees")
-	filter := bson.M{
-		"email": email,
+	userID, _ := primitive.ObjectIDFromHex(id)
+	user := users.User{
+		ID:           userID,
+		EmailAddress: email,
+		LastName:     emp.Name.LastName,
+		FirstName:    emp.Name.FirstName,
+		MiddleName:   emp.Name.MiddleName,
 	}
-	var emp employees.Employee
-	err = empCol.FindOne(context.TODO(), filter).Decode(&emp)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	user.SetPassword("zaq1!QAZzaq1!QAZ")
+	user.Workgroups = append(user.Workgroups, "metrics-GEOINT")
+	user.Workgroups = append(user.Workgroups, "scheduler-employee")
 
-	// Add scheduler-employee to user workgroups
-	if !user.IsInGroup("scheduler", "employee") {
-		user.Workgroups = append(user.Workgroups, "scheduler-employee")
-	}
-	err = svcs.UpdateUser(*user)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	oldKey := emp.ID
-
-	// insert the new employee record
-	emp.ID = user.ID
-	_, err = empCol.InsertOne(context.TODO(), emp)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// delete old employee by id
-	filter = bson.M{
-		"_id": oldKey,
-	}
-	_, err = empCol.DeleteOne(context.TODO(), filter)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// change reference employee id from old key to new key
-	workCol := config.GetCollection(config.DB, "scheduler", "employeework")
-
-	filter = bson.M{
-		"employeeID": oldKey,
-	}
-
-	var work []employees.EmployeeWorkRecord
-	cursor, err := workCol.Find(context.TODO(), filter)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	if err = cursor.All(context.TODO(), &work); err != nil {
-		log.Fatalln(err)
-	}
-	for _, wk := range work {
-		wk.EmployeeID = user.ID
-		filter = bson.M{
-			"_id": wk.ID,
-		}
-		_, err = workCol.ReplaceOne(context.TODO(), filter, wk)
-		if err != nil {
-			log.Println(err)
-		}
-	}
+	userCol := config.GetCollection(config.DB, "authenticate", "users")
+	userCol.InsertOne(context.TODO(), user)
 }
